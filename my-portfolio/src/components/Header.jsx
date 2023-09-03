14f2dc46235bca2613c535de93b19c021271a2fe
Spleentery <davidburchakov@gmail.com>
2023-09-03 03:14:39 +0200

    seems to work

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import 'css/Header.css';
+import logo from 'logo.svg';
 // src/components/Header.jsx
 export default function Header(){
     return(
         <header>
+            <img src={logo} className="App-logo" alt="logo" />
             <nav>
             <a href="/">
                 David Burchakov
