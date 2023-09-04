8874a4262bc66b44c32c24cd20c753ca39f3a2bd
Spleentery <deepbluebirdss@gmail.com>
2023-09-04 18:29:25 +0200

    text mesh

--- my-portfolio/src/sections/Laptop.jsx
+++ my-portfolio/src/sections/Laptop.jsx
@@ -1,6 +1,6 @@
 import 'css/contact.css';
 import { useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Html, OrbitControls, PivotControls } from '@react-three/drei';
 import { useRef } from 'react';
 import { useMemo } from 'react';
 import * as THREE from 'three'
@@ -14,15 +14,31 @@ export default function Contact(){
         groupRef.current.rotation.y += delta
     })
 
-
+    const customControls = {
+        enableDamping: true,
+        mouseButtons: {
+          LEFT: 1,    // Use 2 for middle mouse button
+          MIDDLE: 2,  // Use 1 for left mouse button
+          RIGHT: THREE.MOUSE.ROTATE,   // Use 0 for right mouse button
+        },
+      };
 
 
     return <>
+            <OrbitControls {...customControls}/>
+
+            <PivotControls>
+
+            </PivotControls>
+
             {/* position-y={ 1 } */} 
             <group ref={ groupRef }>
                 <mesh ref={ laptopRef } scale={3} rotation-y={15}>
                     <boxGeometry/>
                     <meshNormalMaterial />
+                    <Html>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus nesciunt optio aut omnis ullam quod reiciendis. Sequi sed nostrum fugiat, veritatis odit corrupti ducimus exercitationem ratione amet officiis animi autem.
+                    </Html>
                 </mesh>
             </group>
     </>
