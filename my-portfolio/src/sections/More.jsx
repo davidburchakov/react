327f2b3bd304a27168bd779606bc53eebe57c4c8
Spleentery <deepbluebirdss@gmail.com>
2023-09-08 01:17:36 +0200

    update

--- my-portfolio/src/sections/More.jsx
+++ my-portfolio/src/sections/More.jsx
@@ -1,4 +1,3 @@
-// import "css/styles.scss";
 import "css/more.css";
 import React, { useRef, useEffect } from "react";
 
@@ -6,42 +5,36 @@ export default function More() {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    loadVideoOnScroll(".more");
+    loadVideoOnScroll(".video-container");
   }, []);
 
-  const loadVideoOnScroll = (containerSelector) => {
-    const container = document.querySelector(containerSelector);
+  const loadVideoOnScroll = (container) => {
+    container = document.querySelector(container);
 
     const videoPlayOnScroll = () => {
       if (videoRef.current.duration) {
-        const sectionHeight = container.clientHeight;
-        const scrollY = window.scrollY - container.offsetTop;
-        const scrollPercent = scrollY / sectionHeight;
-
-        const numberOfFrames = 6;
+        const scrollPercent =
+          window.scrollY / (container.scrollHeight - window.innerHeight);
         videoRef.current.currentTime =
-          videoRef.current.duration * scrollPercent * numberOfFrames;
+          videoRef.current.duration * scrollPercent/2;
       }
       requestAnimationFrame(videoPlayOnScroll);
     };
-
     requestAnimationFrame(videoPlayOnScroll);
   };
 
   return (
-    <section className="section more">
-      <div className="App">
-        <div className="video-container">
-          <div className="video-content">
-            <video
-              preload="auto"
-              ref={videoRef}
-              src="https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4"
-              // src={process.env.PUBLIC_URL + '/wingsuit.mp4'}
-            />
-          </div>
+    <div className="App">
+      <div className="video-container">
+        <div className="video-content">
+          <video
+            preload="auto"
+            ref={videoRef}
+            // src="https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4"
+            src={process.env.PUBLIC_URL + '/ed.mp4'}
+          />
         </div>
       </div>
-    </section>
+    </div>
   );
 }
