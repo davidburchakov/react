00e0c2d4ae52144eb90462dc4eebd6a6150b8c17
Spleentery <deepbluebirdss@gmail.com>
2023-09-08 16:12:03 +0200

    update

--- my-portfolio/src/sections/More.jsx
+++ my-portfolio/src/sections/More.jsx
@@ -30,8 +30,8 @@ export default function More() {
           <video
             preload="auto"
             ref={videoRef}
-            // src="https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4"
-            src={process.env.PUBLIC_URL + '/ed.mp4'}
+            src="https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4"
+            // src={process.env.PUBLIC_URL + '/ed.mp4'}
           />
         </div>
       </div>
