11dfb62530e6369512894c9373b5df4965786695
Spleentery <deepbluebirdss@gmail.com>
2023-09-11 18:54:39 +0200

    books imported

--- my-portfolio/src/components/Books.jsx
+++ my-portfolio/src/components/Books.jsx
@@ -3,34 +3,49 @@ import { useRef, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { View, Preload, useGLTF, useAnimations } from '@react-three/drei'
 import Journal from 'components/Journal.jsx'
+import Dostoyevsky from 'components/Dostoyevsky.jsx'
+import Tolstoy from 'components/Tolstoy.jsx'
+import Nietzsche from 'components/Nietzsche.jsx'
 
 export default function Books() {
   const ref = useRef(null)
-  const box = useRef(null)
-  const box2 = useRef(null)
+  const book1 = useRef(null)
+  const book2 = useRef(null)
+  const book3 = useRef(null)
+  const book4 = useRef(null)
 
   
   return (
     <div ref={ref} className="container">
       <div className="text">
-        <div ref={box} className="view scale" style={{ height: 300 }} />
-        <div ref={box2} className="view scale" style={{ height: 300 }} />
+        <div ref={book1} className="view scale" style={{ height: 300 }} />
+        <div ref={book2} className="view scale" style={{ height: 300 }} />
+        <div ref={book3} className="view scale" style={{ height: 300 }} />
+        <div ref={book4} className="view scale" style={{ height: 300 }} />
       </div>
         
       <Canvas eventSource={ref} className="canvas">
         
-        <View track={box}>
-        <ambientLight intensity={1} />
-        <Journal />
+        <View track={book1}>
+          <ambientLight intensity={1} />
+          <Journal />
+        </View>
 
-        </View>
-        <View track={box2}>
+        <View track={book2}>
             <ambientLight intensity={1} />
-            <mesh position={[1,1,1]}>
-                <boxGeometry />
-                <meshStandardMaterial color="blue" />
-            </mesh>
+            <Dostoyevsky  />
         </View>
+
+        <View track={book3}>
+            <ambientLight intensity={1} />
+            <Tolstoy  />
+        </View>
+
+        <View track={book4}>
+            <ambientLight intensity={1} />
+            <Nietzsche  />
+        </View>
+
         <Preload all />
       </Canvas>
     </div>
---
+++ my-portfolio/src/components/Nietzsche.jsx
@@ -0,0 +1,25 @@
+
+import { useEffect } from 'react'
+import { useGLTF, useAnimations } from '@react-three/drei'
+
+export default function Nietzsche() {
+    const journal = useGLTF(process.env.PUBLIC_URL + '/Nietzsche.glb')
+    const journalAnimations = useAnimations(journal.animations, journal.scene)
+    console.log(journalAnimations.names)
+    
+
+    
+
+    const handleClick = () => {
+        const action = journalAnimations.actions.Animation;
+        
+        if (action) {
+          action.reset().play();
+        }
+      };
+    return  <primitive object={journal.scene} scale={1} rotation={[Math.PI / 2, 0, 0]}
+    onClick={(e) => {e.stopPropagation(); handleClick(); console.log('Journal clicked')}}
+    onPointerEnter={(e) => { document.body.style.cursor = 'pointer'; e.stopPropagation()}}
+    onPointerLeave={(e) => { document.body.style.cursor = 'default'; e.stopPropagation()}}
+    />;
+}
