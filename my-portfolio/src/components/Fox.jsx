eb2210b35f3ba7f533e6fdf4f7d62c30476ecf1e
david burchakov <davidburchakov@gmail.com>
2023-10-04 22:18:29 +0200

    models animated. Are rotating and floating up and down

--- my-portfolio/src/components/Fox.jsx
+++ my-portfolio/src/components/Fox.jsx
@@ -1,11 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 import { useGLTF, useAnimations } from '@react-three/drei'
 
 export default function Fox(props) {
 
-  const journal = useGLTF(process.env.PUBLIC_URL + '/Fox.glb')
+    const journal = useGLTF(process.env.PUBLIC_URL + '/Fox.glb')
+    const foxRef = useRef(null);
+
+    // Use the useFrame hook to update rotation and position on each frame
+    useFrame(({ clock }) => {
+      // Rotate the fox model around the Y-axis
+      foxRef.current.rotation.y += 0.02; 
+      
+      // Float the fox model in the X-axis (up and down)
+      foxRef.current.position.y += Math.sin(clock.elapsedTime) * 0.005;
+    });
     
-    return  <primitive object={journal.scene}
+    return  <primitive ref={foxRef} object={journal.scene}
     position={props.position}
     scale={props.scale}
     rotation={props.rotation}
--- my-portfolio/src/components/Penguine.jsx
+++ my-portfolio/src/components/Penguine.jsx
@@ -1,11 +1,21 @@
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 import { useGLTF, useAnimations } from '@react-three/drei'
 
 export default function Penguine(props) {
     const journal = useGLTF(process.env.PUBLIC_URL + '/Peng.glb')
-    
-    return  <primitive object={journal.scene}
+    const pengRef = useRef(null);
+
+    // Use the useFrame hook to update rotation and position on each frame
+    useFrame(({ clock }) => {
+      // Rotate the fox model around the Y-axis
+      pengRef.current.rotation.y += 0.01; 
+      
+      // Float the fox model in the X-axis (up and down)
+      pengRef.current.position.y += Math.sin(clock.elapsedTime) * 0.005;
+    });
+    return  <primitive ref={pengRef} object={journal.scene}
     scale={props.scale}
     position={props.position}
     rotation={props.rotation}
